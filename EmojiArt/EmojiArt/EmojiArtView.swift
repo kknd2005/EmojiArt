d5d761342bf64c05fba132a088f13216f72fa2cb
kknd2005 <elkyxu@me.com>
2019-03-03 18:28:20 +0800

    first step

---
+++ EmojiArt/EmojiArt/EmojiArtView.swift
@@ -0,0 +1,22 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by xu on 3/3/19.
+//  Copyright © 2019 xu. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    var backgroundImage: UIImage?
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by xu on 3/3/19.
+//  Copyright © 2019 xu. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
