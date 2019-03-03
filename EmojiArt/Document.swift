80637500c5ad9c8db982ea21c8c8e563849df652
kknd2005 <elkyxu@me.com>
2019-03-03 18:09:27 +0800

    Initial Commit

---
+++ EmojiArt/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  EmojiArt
+//
+//  Created by xu on 3/3/19.
+//  Copyright © 2019 xu. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
